b1b8975e366d1a2f4e54ac5a93039c5c28c87043
hencel <zbigniewpiernik@gmail.com>
2021-05-15 15:59:15 +0200

    Form on other component. Comunication between form and app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { FormsModule } from '@angular/forms';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RankingComponent } from './ranking/ranking.component';
 import { TitleComponent } from './ranking/title/title.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     RankingComponent,
-    TitleComponent
+    TitleComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
